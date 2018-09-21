69cfdc17fd309cedd5f23d8be5fbdd8c41144b3c
LTTTTTE <10100aaaaa@gmail.com>
2018-09-21 12:41:50 +0900

    날씨,영화순위,실시간검색어,크롤링,새창,바인딩X

---
+++ DragonApp/DragonApp/HttpRequester.cs
@@ -0,0 +1,154 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.Specialized;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DragonApp
+{
+    public class HttpRequester
+    {
+        private static CookieContainer Cook = new CookieContainer();
+       
+        public static string ExecuteIE(string URL, string METHOD = "POST", string Postdata = "", string Referer = "", string CharacterSet = "", string Content = "application/x-www-form-urlencoded", string Accept = "text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8")
+        {
+
+            try
+            {
+                HttpWebRequest request = (HttpWebRequest)WebRequest.Create(URL);
+                request.Accept = Accept;
+                request.UserAgent = "Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/49.0.2623.87 Safari/537.36";
+                request.Method = METHOD;
+                request.ContentType = Content;
+                request.Referer = Referer;
+                request.ContentLength = Postdata.Length;
+                request.KeepAlive = true;
+                request.CookieContainer = Cook;
+                request.Timeout = 0x7530;
+                if (Postdata.Length > 0)
+                {
+                    TextWriter writer = new StreamWriter(request.GetRequestStream());
+                    writer.Write(Postdata);
+                    writer.Close();
+                }
+                HttpWebResponse response = (HttpWebResponse)request.GetResponse();
+                request.CookieContainer.Add(response.Cookies);
+                if (response.StatusCode == HttpStatusCode.OK)
+                {
+                    TextReader reader;
+                    if (CharacterSet.Length > 0)
+                    {
+                        reader = new StreamReader(response.GetResponseStream(), Encoding.GetEncoding(CharacterSet));
+                    }
+                    else if ((response.CharacterSet == "ms949") | (response.CharacterSet == "MS949"))
+                    {
+                        reader = new StreamReader(response.GetResponseStream(), Encoding.GetEncoding("ks_c_5601-1987"));
+                    }
+                    else if (response.CharacterSet == "")
+                    {
+                        reader = new StreamReader(response.GetResponseStream(), Encoding.GetEncoding("utf-8"));
+                    }
+                    else
+                    {
+                        reader = new StreamReader(response.GetResponseStream(), Encoding.GetEncoding(response.CharacterSet));
+                    }
+                    return reader.ReadToEnd();
+                }
+                request.Abort();
+            }
+            catch (Exception exception1)
+            {
+                return "error\n" + exception1.Message;
+            }
+            return "error";
+        }
+
+        public static string HttpUploadFile(string url, string file, string paramName, string contentType, byte[] filebytes, string referer, NameValueCollection nvc)
+        {
+            string boundary = "----------" + "ae0Ij5ei4cH2ei4Ef1Ef1gL6gL6KM7";
+            byte[] boundarybytes = System.Text.Encoding.ASCII.GetBytes("\r\n--" + boundary + "\r\n");
+
+            HttpWebRequest wr = (HttpWebRequest)WebRequest.Create(url);
+            wr.UserAgent = "Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/49.0.2623.110 정도사파리/537.36";
+            wr.ContentType = "multipart/form-data; boundary=" + boundary;
+            wr.Method = "POST";
+            wr.KeepAlive = true;
+            wr.Referer = referer;
+            wr.AllowWriteStreamBuffering = true;
+            wr.CookieContainer = Cook;
+
+            Stream rs = wr.GetRequestStream();
+
+            string formdataTemplate = "Content-Disposition: form-data; name=\"{0}\"\r\n\r\n{1}";
+            foreach (string key in nvc.Keys)
+            {
+                rs.Write(boundarybytes, 0, boundarybytes.Length);
+                string formitem = string.Format(formdataTemplate, key, nvc[key]);
+                byte[] formitembytes = System.Text.Encoding.UTF8.GetBytes(formitem);
+                rs.Write(formitembytes, 0, formitembytes.Length);
+            }
+            rs.Write(boundarybytes, 0, boundarybytes.Length);
+
+            string headerTemplate = "Content-Disposition: form-data; name=\"{0}\"; filename=\"{1}\"\r\nContent-Type: {2}\r\n\r\n";
+            string header = string.Format(headerTemplate, paramName, file, contentType);
+            byte[] headerbytes = System.Text.Encoding.UTF8.GetBytes(header);
+            rs.Write(headerbytes, 0, headerbytes.Length);
+            rs.Write(filebytes, 0, filebytes.Length);
+
+            byte[] trailer = System.Text.Encoding.ASCII.GetBytes("\r\n--" + boundary + "--\r\n");
+            rs.Write(trailer, 0, trailer.Length);
+            rs.Close();
+
+            HttpWebResponse wresp = (HttpWebResponse)wr.GetResponse();
+            wr.CookieContainer.Add(wresp.Cookies);
+            if (wresp.StatusCode == HttpStatusCode.OK)
+            {
+                TextReader reader = new StreamReader(wresp.GetResponseStream(), Encoding.GetEncoding(wresp.CharacterSet));
+                reader = new StreamReader(wresp.GetResponseStream(), Encoding.GetEncoding("utf-8"));
+                return reader.ReadToEnd();
+            }
+            wr.Abort();
+            return "error";
+        }
+
+
+        public static string ImageUpload(string url, byte[] filebytes, string referer, NameValueCollection ㅈㄷ)
+        {
+            string image01 = "------------ae0Ij5ei4cH2ei4Ef1Ef1gL6gL6KM7\r\nContent-Disposition: form-data; name=\"Filename\"\r\n\r\n파일이름\r\n------------ae0Ij5ei4cH2ei4Ef1Ef1gL6gL6KM7\r\nContent-Disposition: form-data; name=\"imgIndex\"\r\n\r\n0\r\n------------ae0Ij5ei4cH2ei4Ef1Ef1gL6gL6KM7\r\nContent-Disposition: form-data; name=\"fileName\"\r\n\r\n파일이름\r\n------------ae0Ij5ei4cH2ei4Ef1Ef1gL6gL6KM7\r\nContent-Disposition: form-data; name=\"userId\"\r\n\r\n아이디\r\n------------ae0Ij5ei4cH2ei4Ef1Ef1gL6gL6KM7\r\nContent-Disposition: form-data; name=\"image\"; filename=\"파일이름\"\r\nContent-Type: application/octet-stream\r\n\r\n데이터";
+            string image02 = "데이터\r\n------------ae0Ij5ei4cH2ei4Ef1Ef1gL6gL6KM7\r\nContent-Disposition: form-data; name=\"Upload\"\r\n\r\nSubmit Query\r\n------------ae0Ij5ei4cH2ei4Ef1Ef1gL6gL6KM7--";
+            try
+            {
+                HttpWebRequest request = (HttpWebRequest)WebRequest.Create(url);
+                request.UserAgent = "Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/49.0.2623.110 Safari/537.36";
+                request.Method = "Post";
+                request.ContentType = "multipart/form-data; boundary=----------ae0Ij5ei4cH2ei4Ef1Ef1gL6gL6KM7";
+                request.Referer = referer;
+                request.KeepAlive = true;
+                request.AllowWriteStreamBuffering = false;
+                request.CookieContainer = Cook;
+                request.Timeout = 0xea60;
+                request.GetRequestStream().Write(filebytes, 0, filebytes.Length);
+                request.GetRequestStream().Write(Encoding.Default.GetBytes(image02.Replace("데이터", "")), 0, Encoding.Default.GetBytes(image02.Replace("데이터", "")).Length);
+                request.GetRequestStream().Close();
+                HttpWebResponse response = (HttpWebResponse)request.GetResponse();
+                request.CookieContainer.Add(response.Cookies);
+                if (response.StatusCode == HttpStatusCode.OK)
+                {
+                    TextReader reader = new StreamReader(response.GetResponseStream(), Encoding.GetEncoding(response.CharacterSet));
+                    reader = new StreamReader(response.GetResponseStream(), Encoding.GetEncoding("utf-8"));
+                    return reader.ReadToEnd();
+                }
+                request.Abort();
+            }
+            catch (Exception exception1)
+            {
+                return "error" + exception1.Message;
+            }
+            return "error";
+        }
+
+    }
+}
---
+++ DragonApp/DragonApp/MainWindow.xaml.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using Microsoft.VisualBasic;
+namespace DragonApp
+{
+    /// <summary>
+    /// MainWindow.xaml에 대한 상호 작용 논리
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            string IP_load = HttpRequester.ExecuteIE("https://search.naver.com/search.naver?where=nexearch&sm=top_hty&fbm=1&ie=utf8&query=%EB%82%B4+%EC%95%84%EC%9D%B4%ED%94%BC+%ED%99%95%EC%9D%B8", "GET");
+            tb_IP.Text = "IP : " + Strings.Split(Strings.Split(IP_load, "이 컴퓨터의 IP주소는 <em>")[1], "</em>")[0];
+
+            string Weather_Load = HttpRequester.ExecuteIE("https://search.naver.com/search.naver?where=nexearch&sm=top_hty&fbm=1&ie=utf8&query=" + /*weather_selected +*/ "날씨", "GET");
+            var City = Strings.Split(Strings.Split(Weather_Load, "<span class=\"btn_select\" role=\"button\"> <em>")[1], "</em>")[0];
+            tb_city.Text = "오늘 " + City + "의 날씨에요";
+            var Weather = Strings.Split(Strings.Split(Weather_Load, "<p class=\"cast_txt\">")[1], "</p>")[0];
+            tb_weather.Text = "날씨 : " + Weather;
+            var temperature = Strings.Split(Strings.Split(Weather_Load, "<span class=\"todaytemp\">")[1], "</span>")[0];
+            tb_temperature.Text = "기온 : " + temperature + "℃    " + "체감온도 : " + Strings.Split(Strings.Split(Weather_Load, "체감온도 <em><span class=\"num\">")[1], "</span>")[0] + "℃";
+
+            string[] movie = new string[5];
+            TextBlock[] textBlocks_movie = { tb_movie_1, tb_movie_2, tb_movie_3, tb_movie_4, tb_movie_5 };
+            string Movie_load = HttpRequester.ExecuteIE("https://movie.naver.com/movie/sdb/rank/rreserve.nhn", "GET");
+            for (int index = 0; index < movie.Length; index++)
+            {
+                textBlocks_movie[index].Text = movie[index] =string.Format("{0}위 : ",index+1)+ Strings.Split(Strings.Split(Strings.Split(Movie_load, string.Format("alt=\"0{0}\"", index+1))[1], "title=\"")[1], "\">")[0];
+            }
+
+
+            string search_load = HttpRequester.ExecuteIE("https://datalab.naver.com/keyword/realtimeList.naver", "GET");
+            var search_date = Strings.Split(Strings.Split(search_load, "<strong class=\"rank_title v2\">")[1], "<em>")[0];
+            var search_time = Strings.Split(Strings.Split(Strings.Split(search_load, "<strong class=\"rank_title v2\">")[1], "<em>")[1], "</em></strong>")[0];
+            tb_search_date_time.Text = search_date + " " + search_time +"\n   실시간 검색어 순위";
+            tb_movie_date.Text = search_date + "기준 \n영화 예매율 순위";
+
+            string[] search = new string[10];
+            TextBlock[] textBlocks = { tb_search_1, tb_search_2, tb_search_3, tb_search_4, tb_search_5, tb_search_6, tb_search_7, tb_search_8, tb_search_9,tb_search_10 };
+            for (int index = 0; index < search.Length; index++)
+            {
+                textBlocks[index].Text = search[index] =string.Format("{0}위 : ",index+1)+ Strings.Split(Strings.Split(Strings.Split(search_load, string.Format("<em class=\"num\">{0}</em>",index+1))[1], "<span class=\"title\">")[1], "</span>")[0];
+            }
+        }
+
+        private void bt_crawling_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                Window1 window1 = new Window1();
+                window1.Show();
+                string URL = tb_url.Text;
+                string Source = HttpRequester.ExecuteIE(URL, "GET", "", URL);
+                // tb_Test.Text = Source;
+                window1.tb_source.Text = Source;
+            }
+            catch
+            {
+                MessageBox.Show("Exception");
+            }
+
+        }
+
+    }
+}
